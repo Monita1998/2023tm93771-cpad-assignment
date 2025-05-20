f38a9acbfa7cafcd15137b35329e8d9e343e93bf
MonitaMoni132 <MonitaMoni132@gmail.com>
2025-05-20 14:51:31 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Monita Flutter Web test',
+      title: 'Monita Flutter Web',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
